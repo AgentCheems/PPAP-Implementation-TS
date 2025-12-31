843ce348aaa58b84fd3d165063503960028d9160
AgentCheems <lance3232006@gmail.com>
2025-12-31 11:15:22 +0800

    Delete src directory

--- src/message.ts
+++
@@ -1,9 +0,0 @@
-import { Schema as S } from "effect"
-import { CanvasMsg } from "cs12251-mvu/src/canvas"
-
-export type Msg = typeof Msg.Type
-export const Msg = S.Union(
-    // S.TaggedStruct("GameChuChu", {}),
-    CanvasMsg,
-    S.TaggedStruct("Restart", {}),
-)
